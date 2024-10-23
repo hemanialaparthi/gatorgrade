5326349197fd2c38978dc8e1e0256ca6427aada6
hemanialaparthi <alaparthi01@allegheny.edu>
2024-10-23 11:08:23 -0400

    add(display.py): create a dictionary of commands for gatorgrade. this is mocking the execexam commands as working on functionality is my top priority.

--- gatorgrade/output/display.py
+++ gatorgrade/output/display.py
@@ -1,1 +1,14 @@
+from rich.console import Console
 
+
+def display_tldr(console: Console, chosen_command: str = None) -> None:
+    """Display a list of example commands and their descriptions."""
+    # a dictionaly of commands and their descriptions
+    commands = {
+        "mark": "poetry run execexam <path-to-project> <path-to-tests> --maxfail",
+        "report": "poetry run execexam <path-to-project> <path-to-tests> --report <report_type>/<all>",
+        "advice-model": "poetry run execexam <path-to-project> <path-to-tests> --advice-model <model> --advice-method <method>",
+        "debug": "poetry run execexam <path-to-project> <path-to-tests> <--debug>/<--no-debug>",
+        "fancy": "poetry run execexam <path-to-project> <path-to-tests> <--fancy>/<--no-fancy>",
+        "verbose": "poetry run execexam <path-to-project> <path-to-tests> <--verbose>/<--no-verbose>",
+    }
